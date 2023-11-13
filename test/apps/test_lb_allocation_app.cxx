e74ab9138c952611280ee568b4fc420aa29ad094
roland-sipos <sipos.roland.cb@gmail.com>
2023-11-13 15:51:20 +0100

    cleanup test app

--- test/apps/test_lb_allocation_app.cxx
+++ test/apps/test_lb_allocation_app.cxx
@@ -22,10 +22,6 @@
 #include <string>
 #include <vector>
 
-#include <stdio.h>
-#include <unistd.h>
-#include <inttypes.h>
-
 #ifdef WITH_LIBNUMA_SUPPORT
 #include <numaif.h>
 #endif
@@ -51,34 +47,6 @@ namespace {
   bool prefill = false; // Prefill the LB
   std::size_t alignment_size = 4096;
 }
-
-#include "stdio.h"
-#include "unistd.h"
-#include "inttypes.h"
-
-uintptr_t vtop(uintptr_t vaddr) {
-  FILE *pagemap;
-  intptr_t paddr = 0;
-  int offset = (vaddr / sysconf(_SC_PAGESIZE)) * sizeof(uint64_t);
-  uint64_t e;
-
-  // https://www.kernel.org/doc/Documentation/vm/pagemap.txt
-  if ((pagemap = fopen("/proc/self/pagemap", "r"))) {
-      if (lseek(fileno(pagemap), offset, SEEK_SET) == offset) {
-          if (fread(&e, sizeof(uint64_t), 1, pagemap)) {
-              if (e & (1ULL << 63)) { // page present ?
-                  paddr = e & ((1ULL << 54) - 1); // pfn mask
-                  paddr = paddr * sysconf(_SC_PAGESIZE);
-                  // add offset within page
-                  paddr = paddr | (vaddr & (sysconf(_SC_PAGESIZE) - 1));
-              }   
-          }   
-      }   
-      fclose(pagemap);
-  }   
-
-  return paddr;
-}   
 
 int
 main(int argc, char** argv)
